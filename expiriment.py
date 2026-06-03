faa1622dac09f47f19c3aec6f9165b0def622adc
Thulasiram-9 <thulasiguru2006@gmail.com>
2026-06-03 19:25:16 +0530

    Improve input prompt for mass in kinetic energy calculation script create test1 to print greetings

--- expiriment.py
+++ expiriment.py
@@ -1,4 +1,4 @@
-mass=float(input("Enter mass "))
+mass=float(input("Enter the mass "))
 velocity=float(input("Enter velocity "))
 kineticEnergy=((0.5)*mass*velocity**2)
 print(f'Kinetic energy is {kineticEnergy} Joules')
