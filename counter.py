2d30594262ad12b3de48397e003b039346f3b030
Thulasiram-9 <thulasiguru2006@gmail.com>
2026-06-04 21:41:59 +0530

    added comments

--- counter.py
+++ counter.py
@@ -1,5 +1,6 @@
 Num=int(input("Enter a number: "))
 counter=0
-while counter<=Num:
+while counter<=Num:#it will print the numbers ubtill counter reaches the user input number. so that it print the numbers from 0 to the user input number.
     print(counter)
     counter=counter+1
+    
